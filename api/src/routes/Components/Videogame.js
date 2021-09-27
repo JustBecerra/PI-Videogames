65526d7b1ea52a46caae5c8670922f1ba1c1c618
JustBecerra <justj.becerra@gmail.com>
2021-09-27 16:33:22 -0300

    agregue request a la base de datos

--- api/src/routes/Components/Videogame.js
+++ api/src/routes/Components/Videogame.js
@@ -3,13 +3,13 @@ const router = express.Router();// buscar que es Router
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 require('dotenv').config();
 const {YOUR_API_KEY} = process.env
+const {Videogame} = require('../../db')
 
 router.get('/', async (req, res) => {
     try{
       const response = await fetch(`https://api.rawg.io/api/games?key=${YOUR_API_KEY}`);
       const data = await response.json();
-    //   return res.json(data.results)
-      //mapeo para devolver los datos necesarios
+      //filtro para devolver los datos necesarios
       arrData = data.results.map(({name, id, released, rating, platforms}) => ({
         name,
         id,
@@ -17,26 +17,52 @@ router.get('/', async (req, res) => {
         rating,
         platforms
       }));
-
+      let localVG = await Videogame.findAll()
+      console.log(data)
       //si recibo nombre por query
-      if(req.query.name){
-        arrVideoG = arrData.filter(VG => VG.name.includes(req.query.name))
-        arrVideoG.length = 15;
-        return res.json(arrVideoG)
+      arrVideoG = arrData.filter(VG => VG.name.includes(req.query.name))
+      arrLocalVG = localVG.filter(VG => VG.name.includes(req.query.name))
+
+      if(arrData.some(elem => elem.name.includes(req.query.name))){
+        if(arrVideoG.length > 15){
+          arrVideoG.length = 15;
+        }else{
+          let dif = 15 - arrVideoG.length
+          arrLocalVG.length = dif
+        }
+        
+       return res.json([...arrVideoG, ...arrLocalVG])
+      }else if(arrVideoG.every(elem => elem === null)){
+        return res.status(404).send('no se encontro un video juego que contenga esa palabra en su nombre')
       }
-      if(arrVideoG.length === 0 && !arrData.length){
-        return res.status(404).send(`no se encontro un video juego que contenga ${req.query.name} en su nombre`)
-      }
-      
-      return res.json(arrData)
+      return res.json([...arrData, ...localVG])
     }catch(err){
-       res.send(err)
+       return res.send(err)
     }
 })
 
-router.get('/:idVideoGame', (req, res) => {
-    
+router.get('/:id', async (req, res) => {
+    try{
+      const response = await fetch(`https://api.rawg.io/api/games/${req.params.id}?key=${YOUR_API_KEY}`);
+      const data = await response.json();
+      
+      Juego = data.filter(({name, id, released, rating, platforms, genres}) => {
+        name,
+        id,
+        released,
+        rating,
+        platforms,
+        genres
+      })
+      
+
+      Juego = data.filter(elem => elem.id === parseInt(req.params.id))
+      return res.json(Juego)
+    }catch(err){
+      return res.send(err)
+    }
 })
+
 
 
 
